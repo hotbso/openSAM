0b9b0a2782bfe4d43cdcedc55ba6b9321750eb21
Holger Teutsch <holger.a.teutsch@gmail.com>
2024-11-19 10:00:57 +0100

    remove Windows specific path

--- mk_zip.sh
+++ mk_zip.sh
@@ -2,7 +2,7 @@
 set -v
 VERSION=$(cat version.mak |sed -e 's/.*=//')
 
-(cd openSAM-pkg && /c/Program\ Files/7-Zip/7z.exe a "../openSAM-$VERSION.zip" *)
+(cd openSAM-pkg && 7z a "../openSAM-$VERSION.zip" *)
 
 ./sync_xp11_pkg.sh
-(cd openSAM-pkg_XP11 && /c/Program\ Files/7-Zip/7z.exe a "../openSAM_XP11-$VERSION.zip" *)
+(cd openSAM-pkg_XP11 && 7z a "../openSAM_XP11-$VERSION.zip" *)
