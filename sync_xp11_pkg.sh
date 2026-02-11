15bea46f0fd47889ff70c090f5b89fee7d4c46bf
Holger Teutsch <holger.a.teutsch@gmail.com>
2026-02-11 17:15:22 +0100

    support 'blanks in filenames' in sync_xp11_pkg.sh

--- sync_xp11_pkg.sh
+++ sync_xp11_pkg.sh
@@ -8,19 +8,19 @@ cd openSAM-pkg
 find . \! -name '*.xpl' |\
 while read f
 do
-    xp11=$XP11/$f
+    xp11="$XP11/$f"
     if [[ -d $f ]]
     then
-        if [[ ! -d $xp11 ]]; then mkdir -p $xp11; fi
+        if [[ ! -d $xp11 ]]; then mkdir -p "$xp11"; fi
     elif [[ ! -e $xp11 || $xp11 -ot $f ]]
     then
         if [[ $f == *.obj ]]
         then
-            cat $f |\
+            cat "$f" |\
                 sed -e 's/ATTR_light_level/#XP11 ATTR_light_level/g' \
-                    -e 's/GLOBAL_luminance/#XP11 GLOBAL_luminance/g' > $xp11
+                    -e 's/GLOBAL_luminance/#XP11 GLOBAL_luminance/g' > "$xp11"
         else
-            cp -p $f $xp11
+            cp -p "$f" "$xp11"
         fi
     fi
 done
