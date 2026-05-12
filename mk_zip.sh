af99c06130d3a8252ec277d07874fff52e925d88
Holger Teutsch <holger.a.teutsch@gmail.com>
2026-05-12 09:04:47 +0200

    remove obsolete file

--- mk_zip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -v
-VERSION=$(cat version.mak |sed -e 's/.*=//')
-
-(cd openSAM-pkg && 7z a "../openSAM-$VERSION.zip" *)
-
-./sync_xp11_pkg.sh
-(cd openSAM-pkg_XP11 && 7z a "../openSAM_XP11-$VERSION.zip" *)
